5a72065e743ff8f2852c7f2bfcd4125bd03496e1
habi802 <shb8002@gmail.com>
2025-08-06 15:05:15 +0900

    [init] first

---
+++ src/main/java/kr/co/navermap/NavermapApplication.java
@@ -0,0 +1,13 @@
+package kr.co.navermap;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NavermapApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NavermapApplication.class, args);
+    }
+
+}
---
+++ src/test/java/kr/co/navermap/NavermapApplicationTests.java
@@ -0,0 +1,13 @@
+package kr.co.navermap;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NavermapApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

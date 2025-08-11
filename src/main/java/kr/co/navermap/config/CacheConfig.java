9237ba2cdb244548d1dc89fe279d7ebf906d583a
habi802 <shb8002@gmail.com>
2025-08-11 16:41:26 +0900

    [feat] cache config 추가

---
+++ src/main/java/kr/co/navermap/config/CacheConfig.java
@@ -0,0 +1,15 @@
+package kr.co.navermap.config;
+
+import org.springframework.cache.CacheManager;
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@EnableCaching
+public class CacheConfig {
+    @Bean
+    public CacheManager cacheManager() {
+        return null;
+    }
+}

3585902d5e564b0e559abb3602675448dbccb271
habi802 <shb8002@gmail.com>
2025-08-12 09:45:32 +0900

    [feat] implementation caffeiene, springboot cache 추가, 캐시 매니저 추가

--- src/main/java/kr/co/navermap/config/CacheConfig.java
+++ src/main/java/kr/co/navermap/config/CacheConfig.java
@@ -1,15 +1,25 @@
 package kr.co.navermap.config;
 
+import com.github.benmanes.caffeine.cache.Caffeine;
 import org.springframework.cache.CacheManager;
 import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.cache.caffeine.CaffeineCacheManager;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import java.util.concurrent.TimeUnit;
 
 @Configuration
 @EnableCaching
 public class CacheConfig {
     @Bean
     public CacheManager cacheManager() {
-        return null;
+        CaffeineCacheManager cacheManager = new CaffeineCacheManager("direction");
+        cacheManager.setCaffeine(
+            Caffeine.newBuilder()
+                    .expireAfterWrite(1, TimeUnit.DAYS)
+                    .maximumSize(200)
+        );
+        return cacheManager;
     }
 }
